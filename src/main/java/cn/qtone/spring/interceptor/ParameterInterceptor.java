8be38d6366aa21a135e7694b9b8960fe3447a66d
lujunsheng <lujunsheng@8d7e65aa-f446-4ea9-bd95-375d303c44a0>
2014-02-25 01:13:20 +0000

    common-framework-1.0.0

--- src/main/java/cn/qtone/spring/interceptor/ParameterInterceptor.java
+++ src/main/java/cn/qtone/spring/interceptor/ParameterInterceptor.java
@@ -1,13 +1,18 @@
 package cn.qtone.spring.interceptor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import cn.qtone.spring.URICoder;
+import cn.qtone.util.ArrayUtil;
 
 /**
  * 请求参数过滤器(转码&去空格)
@@ -16,15 +21,39 @@ import cn.qtone.spring.URICoder;
  */
 public class ParameterInterceptor extends HandlerInterceptorAdapter {
 
-	@SuppressWarnings("unchecked")
+	private static final Logger logger = LoggerFactory.getLogger(ParameterInterceptor.class);
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+		if (!request.getServletPath().contains(".")) {
+			Map<String, String[]> params = request.getParameterMap();
 
-		Map<String, String[]> params = request.getParameterMap();
+			for (Map.Entry<String, String[]> entry : params.entrySet()) {
+				for (int i = 0; i < entry.getValue().length; i++) {
+					entry.getValue()[i] = URICoder.decode(entry.getValue()[i]).trim();
+				}
+			}
 
-		for (Map.Entry<String, String[]> entry : params.entrySet()) {
-			for (int i = 0; i < entry.getValue().length; i++) {
-				entry.getValue()[i] = URICoder.decode(entry.getValue()[i]).trim();
+			if (logger.isDebugEnabled()) {
+				StringBuilder formatter = new StringBuilder();
+				List<String> values = new ArrayList<String>();
+
+				formatter.append("\n	=> {}");
+				values.add(request.getServletPath());
+
+				String value = null;
+				for (Map.Entry<String, String[]> entry : params.entrySet()) {
+					if (ArrayUtil.length(entry.getValue()) == 1) {
+						value = entry.getValue()[0];
+					} else {
+						value = "[" + ArrayUtil.toString(entry.getValue()) + "]";
+					}
+
+					formatter.append("\n	=> {} : {}");
+					values.add(entry.getKey());
+					values.add(value);
+				}
+				logger.debug(formatter.toString(), values.toArray());
 			}
 		}
 
